0e2df97f9655315bac719aebb2787dc5cdc5a36c
vmalapura <vmalapura@quantumbrains.com>
2024-09-23 16:45:39 +0400

    add ServiceConfig that contain all service\backend settings; disable sync by timer if syncTime = 0; remove store operation with gorb backends; remove modification store operations, now external store available only on RO mode

--- core/store.go
+++ core/store.go
@@ -4,12 +4,11 @@ import (
 	"crypto/tls"
 	"errors"
 	"github.com/qk4l/gorb/local_store"
+	"gopkg.in/yaml.v3"
 	"net/url"
 	"path"
 	"strings"
 	"time"
-
-	"encoding/json"
 
 	"github.com/docker/libkv"
 	"github.com/docker/libkv/store"
@@ -19,6 +18,11 @@ import (
 	"github.com/docker/libkv/store/zookeeper"
 	log "github.com/sirupsen/logrus"
 )
+
+type ServiceConfig struct {
+	ServiceOptions  *ServiceOptions            `yaml:"service_options"`
+	ServiceBackends map[string]*BackendOptions `yaml:"service_backends"`
+}
 
 // StoreSyncStatus info about synchronization with ext-store
 type StoreSyncStatus struct {
@@ -83,30 +87,30 @@ func NewStore(storeURLs []string, storeServicePath, storeBackendPath string, syn
 		hosts = append(hosts, uri.Host)
 	}
 
-	var usedBackend store.Backend
+	var storeBackend store.Backend
 	switch scheme {
 	case "file":
-		usedBackend = "file"
+		storeBackend = "file"
 	case "consul":
-		usedBackend = store.CONSUL
+		storeBackend = store.CONSUL
 	case "etcd":
-		usedBackend = store.ETCD
+		storeBackend = store.ETCD
 	case "zookeeper":
-		usedBackend = store.ZK
+		storeBackend = store.ZK
 	case "boltdb":
-		usedBackend = store.BOLTDB
+		storeBackend = store.BOLTDB
 	case "mock":
-		usedBackend = "mock"
+		storeBackend = "mock"
 	default:
 		return nil, errors.New("unsupported uri scheme : " + scheme)
 	}
-	if usedBackend == "file" {
+	if storeBackend == "file" {
 		kvstore, err = createLocalStore(storePath, storeServicePath, storeBackendPath)
 		if err != nil {
 			return nil, err
 		}
 	} else {
-		kvstore, err = createExtStore(usedBackend, hosts, useTLS)
+		kvstore, err = createExtStore(storeBackend, hosts, useTLS)
 		if err != nil {
 			return nil, err
 		}
@@ -123,21 +127,22 @@ func NewStore(storeURLs []string, storeServicePath, storeBackendPath string, syn
 	context.SetStore(store)
 
 	store.Sync()
-	storeTimer := time.NewTicker(time.Duration(syncTime) * time.Second)
-	go func() {
-		for {
-			select {
-			case <-storeTimer.C:
-				store.Sync()
-			case <-time.After(60 * time.Second):
-				log.Error("Timeout 60s was reached for store.Sync()")
-			case <-store.stopCh:
-				storeTimer.Stop()
-				return
+	if syncTime > 0 {
+		storeTimer := time.NewTicker(time.Duration(syncTime) * time.Second)
+		go func() {
+			for {
+				select {
+				case <-storeTimer.C:
+					store.Sync()
+				case <-time.After(60 * time.Second):
+					log.Error("Timeout 60s was reached for store.Sync()")
+				case <-store.stopCh:
+					storeTimer.Stop()
+					return
+				}
 			}
-		}
-	}()
-
+		}()
+	}
 	return store, nil
 }
 
@@ -181,59 +186,42 @@ func createExtStore(backend store.Backend, hosts []string, useTLS bool) (store.S
 }
 
 func (s *Store) Sync() {
-	services, backends, err := s.getStoreContent()
+	services, err := s.getStoreServices()
 	if err != nil {
 		log.Errorf("error while get data from ext-store: %s", err)
 		return
 	}
 	// synchronize context
-	s.ctx.Synchronize(services, backends)
+	s.ctx.Synchronize(services)
 }
 
 func (s *Store) StoreSyncStatus() (*StoreSyncStatus, error) {
 
-	services, backends, err := s.getStoreContent()
+	services, err := s.getStoreServices()
 	if err != nil {
 		return nil, err
 	}
-	return s.ctx.CompareWithStore(services, backends), nil
+	return s.ctx.CompareWith(services), nil
 }
 
 // StartSyncWithStore synchronize gorb with store
 func (s *Store) StartSyncWithStore() error {
 	// build external services map
-	services, backends, err := s.getStoreContent()
+	services, err := s.getStoreServices()
 	if err != nil {
 		log.Errorf("error while get data from ext-store: %s", err)
 		return err
 	}
 
 	// synchronize context
-	if err = s.ctx.Synchronize(services, backends); err != nil {
+	if err = s.ctx.Synchronize(services); err != nil {
 		return err
 	}
 	return nil
 }
 
-// getStoreContent extract service and backend from ext-store
-func (s *Store) getStoreContent() (map[string]*ServiceOptions, map[string]*BackendOptions, error) {
-	// build external services map
-	services, err := s.getExternalServices()
-	if err != nil {
-		log.Errorf("error while get services form ext-store: %s", err)
-		return nil, nil, err
-	}
-	// build external backends map
-	backends, err := s.getExternalBackends()
-	if err != nil {
-		log.Errorf("error while get backends form ext-store: %s", err)
-		return nil, nil, err
-	}
-	return services, backends, nil
-}
-
-func (s *Store) getExternalServices() (map[string]*ServiceOptions, error) {
-	services := make(map[string]*ServiceOptions)
+func (s *Store) getStoreServices() (map[string]*ServiceConfig, error) {
+	services := make(map[string]*ServiceConfig)
 	// build external service map (temporary all services)
 	kvlist, err := s.kvstore.List(s.storeServicePath)
 	if err != nil {
@@ -247,123 +235,22 @@ func (s *Store) getExternalServices() (map[string]*ServiceOptions, error) {
 			continue
 		}
 		id := s.getID(kvpair.Key)
-		var options ServiceOptions
-		if err := json.Unmarshal(kvpair.Value, &options); err != nil {
+		var options ServiceConfig
+		if err := yaml.Unmarshal(kvpair.Value, &options); err != nil {
 			return nil, err
+		}
+		if options.ServiceOptions == nil {
+			continue
+		} else {
+			options.ServiceOptions.Validate(nil)
 		}
 		services[id] = &options
 	}
 	return services, nil
 }
 
-func (s *Store) getExternalBackends() (map[string]*BackendOptions, error) {
-	backends := make(map[string]*BackendOptions)
-	// build external backend map
-	kvlist, err := s.kvstore.List(s.storeBackendPath)
-	if err != nil {
-		if err == store.ErrKeyNotFound {
-			return backends, nil
-		}
-		return nil, err
-	}
-	for _, kvpair := range kvlist {
-		if kvpair.Value == nil {
-			continue
-		}
-		var options BackendOptions
-		if err := json.Unmarshal(kvpair.Value, &options); err != nil {
-			return nil, err
-		}
-		backends[s.getID(kvpair.Key)] = &options
-	}
-	return backends, nil
-}
-
 func (s *Store) Close() {
 	close(s.stopCh)
-}
-
-func (s *Store) CreateService(vsID string, opts *ServiceOptions) error {
-	// put to store
-	if err := s.put(s.storeServicePath+"/"+vsID, opts, false); err != nil {
-		log.Errorf("error while put service to store: %s", err)
-		return err
-	}
-	return nil
-}
-
-func (s *Store) UpdateService(vsID string, opts *ServiceOptions) error {
-	// put to store
-	if err := s.put(s.storeServicePath+"/"+vsID, opts, true); err != nil {
-		log.Errorf("error while put(update) service to store: %s", err)
-		return err
-	}
-	return nil
-}
-
-func (s *Store) CreateBackend(vsID, rsID string, opts *BackendOptions) error {
-	opts.VsID = vsID
-	// put to store
-	if err := s.put(s.storeBackendPath+"/"+rsID, opts, false); err != nil {
-		log.Errorf("error while put backend to store: %s", err)
-		return err
-	}
-	return nil
-}
-
-func (s *Store) UpdateBackend(vsID, rsID string, opts *BackendOptions) error {
-	opts.VsID = vsID
-	// put to store
-	if err := s.put(s.storeBackendPath+"/"+rsID, opts, true); err != nil {
-		log.Errorf("error while put(update) backend to store: %s", err)
-		return err
-	}
-	return nil
-}
-
-func (s *Store) RemoveService(vsID string) error {
-	if err := s.kvstore.DeleteTree(s.storeServicePath + "/" + vsID); err != nil {
-		log.Errorf("error while delete service from store: %s", err)
-		return err
-	}
-	return nil
-}
-
-func (s *Store) RemoveBackend(rsID string) error {
-	if err := s.kvstore.DeleteTree(s.storeBackendPath + "/" + rsID); err != nil {
-		log.Errorf("error while delete backend from store: %s", err)
-		return err
-	}
-	return nil
-}
-
-func (s *Store) put(key string, value interface{}, overwrite bool) error {
-	// marshal value
-	var byteValue []byte
-	var isDir bool
-	if value == nil {
-		byteValue = nil
-		isDir = true
-	} else {
-		_bytes, err := json.Marshal(value)
-		if err != nil {
-			return err
-		}
-		byteValue = _bytes
-		isDir = false
-	}
-	// check key exist (create if not exists)
-	exist, err := s.kvstore.Exists(key)
-	if err != nil {
-		return err
-	}
-	if !exist || overwrite {
-		writeOptions := &store.WriteOptions{IsDir: isDir, TTL: 0}
-		if err := s.kvstore.Put(key, byteValue, writeOptions); err != nil {
-			return err
-		}
-	}
-	return nil
 }
 
 func (s *Store) getID(key string) string {
